2fe13663f02fb0722d0be39183d2fc9a01686618
Ben Ramchandani <ben.ramchandani@gmail.com>
2020-05-31 10:41:07 +0100

    Version 1.1.5

--- data.lua
+++ data.lua
@@ -9,8 +9,8 @@ data:extend(
             alt_selection_color = {r = 1.0, g = 0.2, b = 0.0, a = 0.2},
             selection_mode = {"any-entity"},
             alt_selection_mode = {"any-entity"},
-            selection_cursor_box_type = "not-allowed",
-            alt_selection_cursor_box_type = "not-allowed",
+            selection_cursor_box_type = "entity",
+            alt_selection_cursor_box_type = "entity",
             subgroup = "tool",
             order = "c[automated-construction]-d[outpost-builder]",
             stack_size = 1
